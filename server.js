a19b50eabb93f01867497a9a7b1c1e5b7fbeb228
Dave <david.halewoodfcc@gmail.com>
2021-04-09 16:30:36 +0100

    refactor to mvc structure and and linting rules

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+// console.log(app.get('env'));
+// console.log(process.env);
 
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Listenning on port.... ${PORT}...`));
